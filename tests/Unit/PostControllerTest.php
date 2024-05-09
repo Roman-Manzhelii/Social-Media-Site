24a33df252ea239c2963606e9bf1eabe062dec69
tony4win <149738877+tony4win@users.noreply.github.com>
2024-05-09 20:45:06 +0100

    Adding 2 Database Tests

--- tests/Unit/PostControllerTest.php
+++ tests/Unit/PostControllerTest.php
@@ -3,6 +3,9 @@
 /**
  * https://www.youtube.com/watch?v=UjA-16diixc
  * php artisan make:test PostControllerTest --unit
+ * 
+ * run single function (https://stackoverflow.com/questions/39118117/how-to-execute-specific-test-class-using-phpunit-in-laravel)
+ * php artisan test --filter test_db_missing
  */
 
 namespace Tests\Unit;
@@ -23,17 +26,39 @@ class PostControllerTest extends TestCase
 
     /**
      * HTTP Unit Test for Register
+     * Will fail if email is already in use
      */
     public function test_register(){
 
         $response = $this->post('/register',[
-            'name' => 'Anton',
-            'email' => 'anton@anton.com',
+            'name' => 'anton',
+            'email' => 'anton@susanton.com',
             'password' => 'testPASS77!',
             'password_confirmation' => 'testPASS77!'
         ]);
 
-        $response->assertRedirect('/');
+        $response->assertRedirect('/posts');
+
+    }
+
+    /**
+     * This passes if tony is in the Database
+     */
+    public function test_db_for_tony(){
+
+        $this->assertDatabaseHas('users',[
+            'name' =>  'tony'
+        ]);
+    }
+
+    /**
+     * Passes if this user doesnt exist
+     */
+    public function test_db_missing(){
+
+        $this->assertDatabaseMissing('users',[
+            'name' => 'this_name_does_not_exist'
+        ]);
 
     }
 
