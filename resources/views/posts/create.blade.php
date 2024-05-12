b590ad396f71e725ba5edc1f126935e9985bb67e
LiverPork <D00264678@student.dkit.ie>
2024-05-13 01:25:15 +0300

    style for post.create

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,19 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="create">
-    <h1>Create Post</h1>
-    <form method="POST" action="{{ route('posts.store') }}" enctype="multipart/form-data">
-        @csrf
-        <div class="mb-3">
-            <label for="description" class="form-label">Description</label>
-            <textarea class="form-control" id="description" name="description" rows="3" required></textarea>
-        </div>
-        <div class="mb-3">
-            <label for="graphicContent" class="form-label">Upload Image/Video</label>
-            <input type="file" class="form-control" id="graphicContent" name="graphicContent" accept="image/*,video/*" required>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-</div>
+<div class="create-container flex justify-center">
+    <div class="create">
+        <h1>Create Post</h1>
+        <form method="POST" action="{{ route('posts.store') }}" enctype="multipart/form-data">
+            @csrf
+            <div class="mb-3">
+                <label for="description" class="form-label">Description</label>
+                <textarea class="form-control" id="description" name="description" rows="3" required></textarea>
+            </div>
+            <div class="mb-3">
+                <label for="graphicContent" class="form-label">Upload Image/Video</label>
+                <input type="file" class="form-control" id="graphicContent" name="graphicContent" accept="image/*,video/*" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</div>    
 @endsection
